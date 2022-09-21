1f137abb659cc66a4ff33068a1b8eadbfc1a711a
Girish <111488101+phyelements-admin@users.noreply.github.com>
2022-09-21 11:28:57 +0530

    saved

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Typography } from "@material-ui/core";
+
+import UploadFiles from "./components/upload-files.component";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="mg20">
+        <Typography variant="h6">Material UI File Upload</Typography>
+      </div>
+      <br/>
+      <UploadFiles />
+       
     </div>
   );
 }
